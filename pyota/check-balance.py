8fd3d756ee2fea7cbdf1ac7f0f823b6b42a069e5
brando14 <ubiot.brando@gmail.com>
2020-07-22 13:07:47 +0000

    codigo para verificar el saldo de tu wallet iota

--- pyota/check-balance.py
+++ pyota/check-balance.py
@@ -2,8 +2,11 @@ from iota import Iota
 import pprint
 
 # This is a demonstration seed, always generate your own seed!
+
+print('Please ingress your iota seed:')
 seed = input(str())
 
+print('Please ingress the address of the node:')
 api = Iota(input(), seed)
 
 
