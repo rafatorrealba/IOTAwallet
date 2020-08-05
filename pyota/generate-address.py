fa3622f6ee8bdf4f9d942a0081340b04af2a388e
brando14 <ubiot.brando@gmail.com>
2020-08-05 17:26:25 +0000

    update

--- pyota/generate-address.py
+++ pyota/generate-address.py
@@ -6,12 +6,12 @@ for a given seed. This is the first available, unused address for this seed.
 from iota import Iota
 
 # This is a demonstration seed, always generate your own seed!
-seed = 'XGIGRHSHIR9YVR9ZGLZHKDIPZRXYOAIGTKRXJZCAGFMMOENIFKWJTJQHVAKT9FFKVBFTMYAIRWAEJAQBK'
+seed = 'LSWXWSUSTKLOUHBMHDM9XBPVAHPPTHA9DAKK9XJLQQSNDJCBJCJAVJCRCJZGVRXIQGFCZPMUFQSTHQQNA'
 
 api = Iota('http://localhost:14265', seed)
 
 # We want the first address for this seed (index 0), make sure it hasn't been spent from before!
-addresses = api.get_new_addresses(index=0, count=1, security_level=2)
+addresses = api.get_new_addresses(index=1, count=1, security_level=2)
 
 address = addresses['addresses'][0]
 
